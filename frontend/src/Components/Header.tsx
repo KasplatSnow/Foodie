deaccf28891215186d50db5bb26d3cf08d5ffb93
itsPixel <brandonllanes16@gmail.com>
2024-10-04 13:29:47 -0700

    Search bar mostly designed

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,16 +9,15 @@ import { Button } from '@mui/material';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  backgroundColor: '#EEEEEE',
   '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: '#D0D0D0',
   },
-  marginRight: theme.spacing(2),
+  marginRight: 0,
   marginLeft: 0,
   width: '100%',
   [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
+    marginLeft: 0,
     width: 'auto',
   },
 }));
@@ -34,7 +33,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
+  color: 'black',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
@@ -48,11 +47,11 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function Header() {
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: 'white', color: 'black' }} >
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          {/* Left side logo */}
           <Typography
             variant="h6"
             noWrap
@@ -62,46 +61,53 @@ export default function Header() {
             Foodie
           </Typography>
 
-          <Box sx={{ flexGrow: 1 }} />
-          <Search>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Search>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Button>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-          </Button>
+          {/* Center search bar */}
+          <Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
+            <Search>
+              <StyledInputBase
+                placeholder="Restaurants..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
 
-          <Box sx={{ flexGrow: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Business
-          </Typography>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            Write a Review
-          </Typography>
+            {/* vertical line divider */ }
+            <Box sx = {{backgroundColor: '#B4B4B4', width: '2px'}}>
 
-          <Box sx={{ flexGrow: 1 }} />
-          <Button variant="outlined">Login</Button>
-          <Button variant="outlined">Sign Up</Button>
+            </Box>
+            <Search>
+              <StyledInputBase
+                placeholder="Address, Zip Code..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Button>
+              <SearchIconWrapper sx = {{backgroundColor: '#0085FF'}}>
+                <SearchIcon sx = {{color: 'white'}} />
+              </SearchIconWrapper>
+            </Button>
+          </Box>
+
+          {/* Right side buttons */}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              Business
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              Write a Review
+            </Typography>
+            <Button variant="outlined">Login</Button>
+            <Button variant="outlined">Sign Up</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
