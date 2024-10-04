c3f01e2bb9fb5e688df9d793dff4c1be1ed2eef6
itsPixel <brandonllanes16@gmail.com>
2024-10-04 14:25:23 -0700

    Incorporate css for margin fix and display size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Hello } from './Components/Users/Hello'
 import SearchPage from './pages/search'
+import './App.css';
 
 const router = createBrowserRouter([
   {
