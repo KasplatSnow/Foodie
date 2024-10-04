d8a2f18687c1ba90997c7188d881cfb2d8e091ef
itsPixel <brandonllanes16@gmail.com>
2024-10-04 14:25:40 -0700

    Layout for search page is set

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,17 +1,28 @@
-import { Box, Grid2 } from "@mui/material"
+import { Box, Divider, Grid2 } from "@mui/material"
 import Header from "../Components/Header"
 
 export default function SearchPage() {
     return (
-        <Box>
-            <Header/>
+        <Box sx = {{height: '100%', display: 'flex', flexDirection: 'column'}}>
+            <Header />
             
-            <Grid2 container spacing={2}>
+            <Grid2 container spacing={2} sx = {{height: '100%'}}>
                 <Grid2 size={6}>
-                    Left
+                    <Box sx = {{height: '100%', display: 'flex', flexDirection: 'column'}}>
+                        <Box sx = {{height: '20%'}}>
+                            Filtering
+                        </Box>
+                        <Divider></Divider>
+                        <Box>
+                            List of restaurants
+                        </Box>
+                    </Box>
                 </Grid2>
+                
                 <Grid2 size={6}>
-                    Right
+                    <Box sx = {{height: '100%', backgroundColor: 'blue'}}>
+                        Map
+                    </Box>
                 </Grid2>
             </Grid2>
         </Box>
