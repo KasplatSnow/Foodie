95f394a92fdf3e68f9d2cd842c330e3f0e10fa2a
itsPixel <brandonllanes16@gmail.com>
2024-10-07 19:25:31 -0700

    Restaurant List search result setup

--- frontend/src/Components/Search/ListItem.tsx
+++ frontend/src/Components/Search/ListItem.tsx
@@ -1,19 +1,47 @@
-import { Box, Grid2 } from "@mui/material"
+import {Box, Grid2, Card } from "@mui/material";
 
-export default function ListItem() {
+interface ListItemProps {
+    index: number;
+    name: string;
+    description: string;
+    img: string;
+}
+
+export default function ListItem({ index, name, description, img }: ListItemProps) {
     return (
-        <Grid2 container spacing={2} sx = {{height: '100%'}}>
-            <Grid2 size={4} sx = {{height: '100%', backgroundColor: 'blue'}}>
-                <Box>
-                    Image of Restaurant
-                </Box>
+        <Card sx = {{height: '100%' }}>
+            <Grid2 container spacing={2} sx={{ height: '100%' }}>
+                {/* Image Section */}
+                <Grid2 size={4} sx={{ maxHeight: '100%', maxWidth: '100%' }}>
+                        <img
+                            src={img}
+                            alt={`${name} image`}
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                objectFit: 'cover' // Ensures the image fills the container
+                            }}
+                        />
+                </Grid2>
+
+                {/* Text Section */}
+                <Grid2 size={8}>
+                    <Box>
+                        <h3>{index + 1}. {name}</h3>
+                        <p>Rating</p>
+                        <p>Categories</p>
+                        <p 
+                            style={{
+                                overflow: 'hidden',      // Hide any overflowing text
+                                textOverflow: 'ellipsis', // Add "..." when text overflows
+                                whiteSpace: 'nowrap',     // Prevent text from wrapping to the next line
+                            }}
+                        >
+                            {description}
+                        </p>
+                    </Box>
+                </Grid2>
             </Grid2>
-            
-            <Grid2 size={8}>
-                <Box sx = {{height: '100%'}}>
-                    Restaurant info
-                </Box>
-            </Grid2>
-        </Grid2>
-    )
+        </Card>
+    );
 }
--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -6,14 +6,17 @@ const testList = [
     {
       name: 'test1',
       description: 'Place that sells food',
+      image: 'https://content.sportslogos.net/logos/34/828/full/san_jose_state_spartans_logo_primary_dark_2018_sportslogosnet-9968.png',
     },
     {
       name: 'test2',
       description: 'Another place that sells food',
+      image: 'https://content.sportslogos.net/logos/34/828/full/san_jose_state_spartans_logo_primary_dark_2018_sportslogosnet-9968.png',
     },
     {
       name: 'test3',
-      description: 'Last place',
+      description: 'Last place Last placeLast placeLast placeLast placeLast placeLast placeLast placeLast placeLast placeLast placeLast placeLast place',
+      image: 'https://content.sportslogos.net/logos/34/828/full/san_jose_state_spartans_logo_primary_dark_2018_sportslogosnet-9968.png',
     }
 ];
 
@@ -34,7 +37,7 @@ export default function SearchPage() {
                         <Box sx={{ height: '80%', overflowY: 'auto' }}>
                         {testList.map((item, index) => (
                             <ListItemButton component="a" href="#simple-list" sx = {{ height: '35%' }}>
-                                <ListItem />
+                                <ListItem index = {index} name = {item.name} description = {item.description} img = {item.image} />
                             </ListItemButton>
                         ))}
                         </Box>
