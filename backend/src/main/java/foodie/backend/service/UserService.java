948f2ebda1b98ca893ca8dad521f13d099899dc1
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-11-25 12:25:53 -0800

    Adding UserService to Service

---
+++ backend/src/main/java/foodie/backend/service/UserService.java
@@ -0,0 +1,30 @@
+package foodie.backend.service;
+import foodie.backend.model.User;
+import foodie.backend.repository.UserRepository;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    } 
+    
+    // Create a new user
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUser(String username){
+        return userRepository.findByUsername(username);
+    }
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
