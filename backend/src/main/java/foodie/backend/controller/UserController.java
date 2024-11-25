973fefe5cc1ce53c490f4c7a8dc864cd0fe2b249
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-11-25 12:34:29 -0800

    Update UserController.java

--- backend/src/main/java/foodie/backend/controller/UserController.java
+++ backend/src/main/java/foodie/backend/controller/UserController.java
@@ -2,16 +2,33 @@ package foodie.backend.controller;
 
 import foodie.backend.model.User;
 import foodie.backend.repository.UserRepository;
+import foodie.backend.service.UserService;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
   @Autowired
-  private UserRepository userService;
+  private final UserService userService;
 
-  @GetMapping("/users")
+  public UserController(UserService userService){
+    this.userService = userService;
+  }
+
+  @PostMapping("/create")
+  public User createUser(@RequestBody User user) {
+      return userService.createUser(user);
+  }
+  @GetMapping("/allusers")
   List<User> getAllUsers() {
-    return userService.findAll();
+    return userService.getAllUsers();
   }
 }
