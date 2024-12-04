02862f7b453a5203ef09499873972d6eb33c6cf1
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-04 15:18:33 -0800

    Update BusinessOwnerRepository.java
    fixed dependencies

--- backend/src/main/java/foodie/backend/repository/BusinessOwnerRepository.java
+++ backend/src/main/java/foodie/backend/repository/BusinessOwnerRepository.java
@@ -1,6 +1,10 @@
 package foodie.backend.repository;
 
+import java.util.List;
+
 import foodie.backend.model.BusinessOwner;
+import foodie.backend.model.Restaurant;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
