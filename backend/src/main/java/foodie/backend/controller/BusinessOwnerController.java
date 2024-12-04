cba6fa582b4504682528bf39dfd0e415c37cc9b2
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-04 13:44:16 -0800

    Update BusinessOwnerController.java
    added methods to retrieve businessOwner restaurants by restaurantID and retreive businessOwners by userID

--- backend/src/main/java/foodie/backend/controller/BusinessOwnerController.java
+++ backend/src/main/java/foodie/backend/controller/BusinessOwnerController.java
@@ -8,4 +8,39 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/business-owner")
-public class BusinessOwnerController {}
+public class BusinessOwnerController {
+  
+    @GetMapping("/getrestaurants/userID/{userID}")
+    public List<RestaurantDTO> getRestaurantsByID(@PathVariable("userID") Long userID) {
+        List<Restaurant> restaurants = businessOwnerService.getRestaurantByID(userID);
+
+        return restaurants.stream().map(restaurant -> new RestaurantDTO(
+            restaurant.getRestaurantID(),
+            restaurant.getName(),
+            restaurant.getAddress(),
+            restaurant.getZipCode(),
+            restaurant.getPhoneNumber(),
+            restaurant.getEmail(),
+            restaurant.getCuisine(),
+            restaurant.getHours(),
+            restaurant.getDescription(),
+            restaurant.getRating(),
+            restaurant.getPrice(),
+            restaurant.getOwnerID(),
+            restaurant.getReviewID())).collect(Collectors.toList());
+    }
+
+    //gets ALL BusinessOwner details
+    //check if has userID property(if wanting to check properties)
+    @GetMapping("/getbusinessowner/userID/{userID}")
+    public List<BusinessOwnerDTO> getBusinessOwnerByID(@PathVariable("userID") Long userID) {
+        List<BusinessOwner> businessOwners = businessOwnerService.getBusinessOwnerByID(userID);
+        return businessOwners.stream().map(businessOwner -> new BusinessOwnerDTO(
+            businessOwner.getUserID(),
+            businessOwner.getUsername(),
+            businessOwner.getAddress(),
+            businessOwner.getPhoneNumber(),
+            businessOwner.getEmail(),
+            businessOwner.getRestaurantID())).collect(Collectors.toList());
+    }
+}
