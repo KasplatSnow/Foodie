376a793bdb533b23251e8ca91159b6030e2d5990
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-04 13:13:32 -0800

    Update BusinessOwnerRepository.java
    Added @Repository and query methods for finding Owner details

--- backend/src/main/java/foodie/backend/repository/BusinessOwnerRepository.java
+++ backend/src/main/java/foodie/backend/repository/BusinessOwnerRepository.java
@@ -3,10 +3,17 @@ package foodie.backend.repository;
 import foodie.backend.model.BusinessOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
-public interface BusinessOwnerRepository
-  extends JpaRepository<BusinessOwner, Long> {
+public interface BusinessOwnerRepository extends JpaRepository<BusinessOwner, Long> {
   BusinessOwner findByEmail(String email);
   boolean existsByEmail(String email);
+    
+  @Query("SELECT r FROM Restaurant r WHERE r.owner.userID = :userID")
+  List<Restaurant> findRestaurantByOwnerID(@Param("userID") Long userID);
+
+  @Query("SELECT b FROM User b WHERE b.role = BUSINESS AND b.userID = :userID")
+  List<BusinessOwner> findBusinessOwnerByID(@Param("userID") Long userID);
 }
