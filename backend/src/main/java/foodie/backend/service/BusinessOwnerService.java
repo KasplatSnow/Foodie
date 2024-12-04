ccb81fe69fb5dce96675a934744d89060e3b3cb6
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-04 15:21:15 -0800

    Update BusinessOwnerService.java
    added missing depencency

--- backend/src/main/java/foodie/backend/service/BusinessOwnerService.java
+++ backend/src/main/java/foodie/backend/service/BusinessOwnerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import foodie.backend.model.Restaurant;
 import foodie.backend.model.BusinessOwner;
 import foodie.backend.repository.BusinessOwnerRepository;
 
