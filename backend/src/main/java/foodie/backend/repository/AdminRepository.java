f66d389c25beee68b27656bc2e20b0782e2246db
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-04 15:44:14 -0800

    Update AdminRepository.java

--- backend/src/main/java/foodie/backend/repository/AdminRepository.java
+++ backend/src/main/java/foodie/backend/repository/AdminRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-  Admin findByID(Long id);
 
   Admin findByEmail(String email);
 
