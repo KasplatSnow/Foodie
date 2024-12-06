273404143fe18b35114406391d3249b9a05c6a03
itsPixel <brandonllanes16@gmail.com>
2024-12-06 14:26:16 -0800

    flyway config

---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE user (
+  userID int NOT NULL AUTO_INCREMENT,
+  username varchar(45) NOT NULL,
+  role enum('USER','BUSINESS','ADMIN') NOT NULL,
+  password varchar(45) NOT NULL,
+  email varchar(45) DEFAULT NULL,
+  address varchar(45) DEFAULT NULL,
+  phone_number varchar(45) DEFAULT NULL,
+  PRIMARY KEY (userID)
+);
+
+CREATE TABLE restaurant (
+  restaurantID int NOT NULL AUTO_INCREMENT,
+  name varchar(255) DEFAULT NULL,
+  ownerID int NOT NULL,
+  address varchar(45) DEFAULT NULL,
+  zip_code varchar(45) DEFAULT NULL,
+  phone_number varchar(45) DEFAULT NULL,
+  cuisine varchar(45) DEFAULT NULL,
+  hours varchar(45) DEFAULT NULL,
+  description varchar(45) DEFAULT NULL,
+  rating float DEFAULT NULL,
+  price int DEFAULT NULL,
+  email varchar(45) DEFAULT NULL,
+  PRIMARY KEY (restaurantID),
+  KEY userID_idx (ownerID),
+  CONSTRAINT userID FOREIGN KEY (ownerID) REFERENCES user (userID)
+);
+
+CREATE TABLE review (
+  reviewID int NOT NULL AUTO_INCREMENT,
+  review_text text NOT NULL,
+  rating float NOT NULL,
+  userID int DEFAULT NULL,
+  restaurantID int DEFAULT NULL,
+  PRIMARY KEY (reviewID),
+  KEY userID (userID),
+  KEY restaurantID (restaurantID),
+  CONSTRAINT review_ibfk_1 FOREIGN KEY (userID) REFERENCES user (userID) ON DELETE CASCADE,
+  CONSTRAINT review_ibfk_2 FOREIGN KEY (restaurantID) REFERENCES restaurant (restaurantID) ON DELETE CASCADE
+);
---
+++ backend/src/main/resources/db/migration/V2__add.sql
@@ -0,0 +1,5 @@
+insert into user (email, username, password, role, phone_number)
+values ("test@gmail.com", "testuser", "test", "BUSINESS", "1231231234");
+
+insert into restaurant (name, ownerID, address, zip_code, phone_number, cuisine, hours, description, rating, price, email)
+values ("testrestaurant", 1, "1234 test avenue", "12345", "7897897890", "bland", "3", "test restaurant", 5.0, 3, "test@gmail.com");
