2c0698be49024d7b532b26e74f149826bde2a817
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-06 06:15:24 -0800

    Update ReviewRepository.java

--- backend/src/main/java/foodie/backend/repository/ReviewRepository.java
+++ backend/src/main/java/foodie/backend/repository/ReviewRepository.java
@@ -1,5 +1,10 @@
 package foodie.backend.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>{
 
