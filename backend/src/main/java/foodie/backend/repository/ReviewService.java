9fea2e0be3ad78bbf32e67123999f2090ca36ad7
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-06 06:15:49 -0800

    Add files via upload

---
+++ backend/src/main/java/foodie/backend/repository/ReviewService.java
@@ -0,0 +1,38 @@
+package foodie.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository){
+        this.reviewRepository = reviewRepository;
+    }
+    
+    // Create a new user
+    public Review createReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Review getById(Long reviewId){
+        return reviewRepository.findByID(reviewId);
+    }
+
+    public List<Review> getReviewByRestaurantID(Long restaurantId){
+        return reviewRepository.findAllRestaurantReviews(restaurantId);
+    }
+
+    public List<Review> getAllUsers(){
+        return reviewRepository.findAll();
+    }
+
+    public boolean getReviewExist(Long restaurantID, Long userID){
+        return reviewRepository.findReviewExist(restaurantID, userID) == null;
+    }
+}
