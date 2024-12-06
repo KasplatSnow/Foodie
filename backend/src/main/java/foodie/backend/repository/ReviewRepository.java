a33b2bad7af3b694156437de6491b001ad0118f4
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-06 06:03:39 -0800

    Update ReviewRepository.java
    Added and corrected ReviewRepository

--- backend/src/main/java/foodie/backend/repository/ReviewRepository.java
+++ backend/src/main/java/foodie/backend/repository/ReviewRepository.java
@@ -1,3 +1,15 @@
 package foodie.backend.repository;
 
-public class ReviewRepository {}
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+    @Query("SELECT r FROM Review r WHERE r.restaurant.restaurantID = :restaurantID AND r.user.userID = :userID")
+    Review findReviewExist(@Param("restaurantID") Long restaurantID, @Param("userID") Long userID);
+
+    //find single review
+    @Query("SELECT r FROM Review r WHERE r.reviewID = :reviewID")
+    Review findByID(@Param("reviewID") Long reviewID);
+
+    //find all reviews of restaurant
+    @Query("SELECT r FROM Review r WHERE r.restaurant.restaurantID = :restaurantID")
+    List<Review> findAllRestaurantReviews(@Param("restaurantID") Long restaurantID);
