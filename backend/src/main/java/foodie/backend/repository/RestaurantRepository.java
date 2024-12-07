b5bf90a7aa5bfaed543b3eb1aa89166a24b4a866
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-07 13:04:57 -0800

    Update RestaurantRepository.java

--- backend/src/main/java/foodie/backend/repository/RestaurantRepository.java
+++ backend/src/main/java/foodie/backend/repository/RestaurantRepository.java
@@ -28,21 +28,10 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
   List<Restaurant> findByAddressContainingIgnoreCase(String address);
 
-  List<Restaurant> findByNameContainingIgnoreCaseOrCuisineContainingIgnoreCase(String name, String cuisine);
-
   @Query("SELECT r FROM Restaurant r WHERE r.restaurantID = :restaurantID")
   Restaurant findByRestaurantID(@Param("restaurantID") Long restaurantID);
   
   @Query("SELECT r FROM Restaurant r WHERE r.name = :name AND r.address = :address")
   List<Restaurant> existsByNameContainingIgnoreCaseAndAddressContainingIgnoreCase(@Param("name") String name, @Param("address") String address);
 
-  // Check if a restaurant exists by ID
-  //boolean existsById(Long id);
-
-  //List<Restaurant> findByBusinessOwner(BusinessOwner businessOwner);
-
-  //implement
-  //List<Restaurant> checkForDuplicateListings();
-
-  //void removeClosedRestaurant(Long restaurantId);
 }
