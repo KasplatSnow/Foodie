6b715c8ad05c883e33a630ab20d25b8506d4deef
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-07 23:18:23 -0800

    Update UserService.java

--- backend/src/main/java/foodie/backend/repository/UserService.java
+++ backend/src/main/java/foodie/backend/repository/UserService.java
@@ -29,7 +29,7 @@ public class UserService {
         return userRepository.findAll();
     }
     
-    public List<User> getUserByID(Long userId){
+    public User getUserByID(Long userId){
         return userRepository.findByID(userId);
     }
 
