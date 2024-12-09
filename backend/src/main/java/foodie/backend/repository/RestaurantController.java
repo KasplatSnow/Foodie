5a7a423cbaf72c7c29a28edff977593b7f4e224b
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-08 17:34:17 -0800

    Update RestaurantController.java
    added setOwnerForRestaurant

--- backend/src/main/java/foodie/backend/repository/RestaurantController.java
+++ backend/src/main/java/foodie/backend/repository/RestaurantController.java
@@ -181,4 +181,10 @@ public class RestaurantController {
         restaurant.getLat(),
         restaurant.getReviewID())).collect(Collectors.toList());
     }
+    
+    @PutMapping("setOwner/businessOwnerId/{businessOwnerID}/restaunantID/{restaurantID}")
+    public ResponseEntity<?> setOwnerForRestaurant(@PathVariable Long businessOwnerID, @PathVariable Long restaurantID) {
+        restaurantService.setRestaurantOwner(businessOwnerID, restaurantID);        
+        return ResponseEntity.ok("Set BusinessOwner");
+    }
 }
