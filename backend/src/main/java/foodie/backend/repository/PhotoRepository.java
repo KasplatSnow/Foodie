0c33db9751ab437108c5d7b74e6a1f06103f152a
Cameron Lee <102389750+KasplatSnow@users.noreply.github.com>
2024-12-09 01:33:44 -0800

    Update PhotoRepository.java

--- backend/src/main/java/foodie/backend/repository/PhotoRepository.java
+++ backend/src/main/java/foodie/backend/repository/PhotoRepository.java
@@ -10,10 +10,9 @@ import org.springframework.data.jpa.repository.Modifying;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM Photo WHERE restaurant.restaurantID = :restaurantID AND photo = :photoToRemove")
-    public void deleteByRestaurantID(Long restaurantID, String photoToRemove);
+    
+    @Query("SELECT photo FROM Photo WHERE restaurant.restaurantID = :restaurantID")
+    List<String> findByRestaurantID(@Param("restaurantID") Long restaurantID);
     
     @Modifying
     @Transactional
